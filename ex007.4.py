14a3c153b3630d87d3cb0b0d6386b7709a573878
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2022-08-05 16:43:14 -0300

    ex de descobrir o maior e menor numero
    entre 3 numeros corrigido usando if e else

--- ex007.4.py
+++ ex007.4.py
@@ -1,20 +1,30 @@
 from time import sleep
+
+maior = 0
+menor = 0
+
 n1 = int(input('Digite o 1° número: '))
 n2 = int(input('Digite o 2° número: '))
 n3 = int(input('Digite o 3° número: '))
-maior = n1
-menor = n1
-
+   
 print(f'Analisando os números...')
 sleep(1.5)
 
-if n2>maior:
+#descobrindo o maior
+if n1 > n2 and n1 > n3:
+    maior = n1
+elif n2 > n3 and n2 > n1:
     maior = n2
-if n3>maior:
+else:
     maior = n3
 print(f'O {maior} é o maior')
-if n2<n1:
+
+
+#descobrindo o menor
+if n1 < n2 and n1 < n3:
+    menor = n1
+elif n2 < n3 and n2 < n1:
     menor = n2
-if n3<n2:
+else:
     menor = n3
 print(f'O {menor} é o menor')
