b4eac64e9ac89bb53fff474272b6c2c31cd84b47
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2022-11-03 15:15:05 -0300

    Update carro.py

--- classes/classe_carro/carro.py
+++ classes/classe_carro/carro.py
@@ -1,126 +1,201 @@
 import inquirer
-from carro import Carro, Veiculo, Menu
-from carro import Ia
+import sys
 
-#colocar formula de velocidade por distancia para obter tempo de viagem
-#consertar abastecimento que pede o argumento carro
-#criar fórmula de gasolina por km/l
-#arrumar os erros do valor na carteira para não ser negativo
+class Veiculo():
+    def __init__(self, marca, modelo, placa, consumo, nivelCombustivel):
+        self.marca = marca
+        self.modelo = modelo
+        self.placa = placa
+        self.consumo = consumo
+        self.nivelCombustivel = nivelCombustivel 
+   
 
-marca_opcoes = [
-        inquirer.List(
-            'escolha',
-            message = 'MARCA DO CARRO',
-            choices = ('Toyota', 'Chevrolet', 'Ford', 'Fiat', 'Volkswagen')
-        )
-    ]
-marca = inquirer.prompt(marca_opcoes)['escolha']
+class Carro(Veiculo):
+    def __init__(self, categoria, airbags, litrosPortaMala, conversivel, marca, modelo, placa, consumo, nivelCombustivel):
+        super().__init__(marca, modelo, placa, consumo, nivelCombustivel)
+        self.categoria = categoria
+        self.airbags = airbags
+        self.litrosPortaMala = litrosPortaMala
+        self.conversivel = conversivel
 
-marcas_modelos_correspondentes = {
-    'Toyota': ('Corolla',  'RAV4', 'Hilux', 'Camry', 'Prius'),
-    'Chevrolet': ('Camaro', 'Cruze', 'Onix', 'Tracker', 'Spin'),
-    'Ford': ('Ka', 'Ranger', 'Fiesta', 'Ecosport', 'Edge'),
-    'Fiat': ('Mobi', 'Argo', 'Pulse', 'Cronos', 'Fiorino'),
-    'Volkswagen': ('Polo', 'Jeta', 'Gol', 'Voyage', 'Fox')
-}
+    def resumo_carro(self):
+        print(f'''
+\033[1;34mMarca:\033[0;0m \033[1;32m{self.marca}\033[0;0m
+\033[1;34mModelo:\033[0;0m \033[1;32m{self.modelo}\033[0;0m
+\033[1;34mPlaca:\033[0;0m \033[1;32m{self.placa}\033[0;0m
+\033[1;34mConsumo (km/L):\033[0;0m \033[1;32m{self.consumo}\033[0;0m
+\033[1;34mTanque:\033[0;0m \033[1;32m{self.nivelCombustivel}\033[0;0m
+\033[1;34mCategoria:\033[0;0m \033[1;32m{self.categoria}\033[0;0m
+\033[1;34mQuantidade Airbags:\033[0;0m \033[1;32m{self.airbags}\033[0;0m
+\033[1;34mPorta-mala (l):\033[0;0m \033[1;32m{self.litrosPortaMala}\033[0;0m
+\033[1;34mConversível?\033[0;0m \033[1;32m{self.conversivel}\033[0;0m
+''')
 
-opcoes_modelo = [
-        inquirer.List(
-            'escolha',
-            message = 'ESCOLHA O MODELO',
-            choices = marcas_modelos_correspondentes[marca]
-        )
-    ]
+
+class Ia:
+    def __init__(self, running, distancia, velocidade):
+        self.running = running
+        self.distancia = distancia
+        self.velocidade = velocidade
+
+    def calculo_tempo():
+        pass
+
+
+class Menu():
+    multa = False
     
-modelo_escolhido = inquirer.prompt(opcoes_modelo)['escolha']
+    def __init__(self, carro, ia):
+        self.carro = carro
+        self.ia = ia
+        self.carteira = 500
 
-placa = input('Digite a placa do carro: ').upper()
-
-opcoes_carro = [
-    inquirer.List(
-        'consumo',
-        message = 'CONSUMO DO CARRO (km/L)',
-        choices = ('8km/L', '10km/L', '13km/L', '15km/L', '18km/L', '20km/L', '22km/L', '25km/L')
-    ),
-    inquirer.List(
-        'tanque',
-        message = 'LITROS DO TANQUE',
-        choices = (25, 30, 35, 40, 50)
-    ),
-    inquirer.List(
-        'categoria',
-        message = 'CATEGORIA DO CARRO',
-        choices = ('Hatch', 'Sedan', 'SUV', 'Pick-up')
-    ),
-    inquirer.List(
-        'airbag',
-        message = 'QUANTIDADE DE AIRBAGS',
-        choices = (1, 2, 3, 4)
-    ),
-    inquirer.List(
-        'porta_mala',
-        message = 'QUANTIDADE DE LITROS NO PORTA MALA',
-        choices = (150, 200, 250, 300, 400, 500, 700)
-    ),
-    inquirer.List(
-        'conversivel',
-        message = 'CONVERSÍVEL?',
-        choices = ('SIM', 'NÃO')
-    )
-]
-
-escolhas_carro = inquirer.prompt(opcoes_carro)
-
-
-carro = Carro(
-    marca=marca, modelo=modelo_escolhido,
-    placa=placa, consumo=escolhas_carro['consumo'],
-    nivelCombustivel=escolhas_carro['tanque'],
-    categoria=escolhas_carro['categoria'],
-    airbags=escolhas_carro['airbag'],
-    litrosPortaMala=escolhas_carro['porta_mala'],
-    conversivel=escolhas_carro['conversivel']
-)
-
-carro.resumo_carro()
-
-cidades = ['Campinas', 'Valinhos', 'Hortolândia', 'Sumaré', 'Vinhedo']
-
-cidades_kilometragem = {
-    'Campinas': 100,
-    'Valinhos': 50,
-    'Hortolândia': 150,
-    'Sumaré': 200,
-    'Vinhedo': 0
-}
-
-saida_opcoes = [
-        inquirer.List(
-            'escolha',
-            message = 'CIDADE ATUAL',
-            choices = cidades
-        )
-    ]
-cidade_saida = inquirer.prompt(saida_opcoes)['escolha']
-kilometragem_cidade_saida = cidades_kilometragem[cidade_saida]
-cidades.remove(cidade_saida)
-
-destino_opcoes = [
-        inquirer.List(
-            'escolha',
-            message = 'CIDADE DESTINO',
-            choices = cidades
-        )
-    ]
+    def start(self):
+        self.menu()
     
-cidade_chegada = inquirer.prompt(destino_opcoes)['escolha']
-kilometragem_cidade_chegada = cidades_kilometragem[cidade_chegada]
+    def menu(self):
+        opcoes = [
+        inquirer.List(
+            'escolha',
+            message = 'MENU DE OPÇÕES',
+            choices = ('Acelerar', 'Desacelerar', 'Manutenir', 'Finanças', 'Abastecer', 'Sair')
+            )
+        ]
+
+        respostas = inquirer.prompt(opcoes)
+
+        if respostas['escolha'] == 'Acelerar':
+            self.acelerar()
+
+        elif respostas['escolha'] == 'Desacelerar':
+            self.desacelerar()
+        
+        elif respostas['escolha'] == 'Manutenir':
+            self.manutenir()
+
+        elif respostas['escolha'] == 'Finanças':
+            self.financas()
+        
+        elif respostas['escolha'] == 'Abastecer':
+            self.abastecer()
+
+        elif respostas['escolha'] == 'Sair':
+            self.sair()
+            
+            
+    def acelerar(self):
+        
+        self.ia.velocidade += 10
+        print('Sua velocidade foi aumentada em 10km/h')
+        print(f'Velocidade atual: {self.ia.velocidade}km/h')
+        if self.ia.velocidade > 80:
+            print('Atenção! Você está na velocidade máxima permitida na rodovia. CUIDADO')
+        self.menu()
 
 
-distancia_cidades = kilometragem_cidade_saida - kilometragem_cidade_chegada
-print(f'A distância das cidades é de {distancia_cidades}km')
+    def desacelerar(self):
+        if self.ia.velocidade == 0:
+            print('A velocidade já está em 0km/h!')
+        else:
+            self.ia.velocidade -= 10
+            print('Sua velocidade foi reduzida em 10km/h')
+            print(f'Velocidade atual: {self.ia.velocidade}')
+        self.menu()
 
-ia = Ia(distancia=distancia_cidades, running=False, velocidade=0)
 
-jogo = Menu(carro=carro, ia=ia)
-jogo.start()
+    def manutenir(self):
+        print('Indo ao mecânico!')
+        opcoes_mecanico = [
+            'Trocar pneus - R$300,00', 
+            'Trocar óleo - R$50,00', 
+            'Lavagem completa - R$150,00', 
+            'Calibragem - R$30,00', 'Voltar'
+        ]
+        
+        valores = {
+            'Trocar pneus - R$300,00': 300,
+            'Trocar óleo - R$50,00': 50,
+            'Lavagem completa - R$150,00': 150,
+            'Calibragem - R$30,00': 30, 
+            'Voltar': 0
+        }
+        
+        opcoes = [
+        inquirer.List(
+            'escolha',
+            message = 'MECÂNICO',
+            choices = opcoes_mecanico
+            )
+        ]
+
+        escolha = inquirer.prompt(opcoes)['escolha']
+
+        if escolha == 'Voltar':
+            return self.menu()
+
+        valor = valores[escolha]
+        if valor > self.carteira:
+            print('Valor insuficiente na carteira para efetuar pagamento!')
+        else:
+            print('Pagamento efetuado com sucesso!')
+            self.carteira -= valor
+            print(f'Valor na carteira: R$ {self.carteira},00')
+        
+        self.menu()
+
+    def abastecer(self, carro:Carro):
+        print('BEM-VINDO AO POSTAY ABASTECIMENTOS')
+        print('>>> R$3,50l')
+
+        litros_abastecer = [
+            inquirer.List(
+            'abastecer',
+            message = 'ESCOLHA A QUANTIDADE DE LITROS',
+            choices = (2, 4, 5, 8, 10, 15)
+        )]
+        escolha = inquirer.prompt(litros_abastecer)['abastecer']
+
+        valor_abastecimento = escolha*3.50 
+        print(f'Você quer abastecer {escolha} litros, valor total: R${valor_abastecimento}')
+        if carro.nivelCombustivel > escolha:
+            if self.carteira > valor_abastecimento:
+                self.carteira -= valor_abastecimento
+                carro.nivelCombustivel += escolha
+                print('Abastecido com sucesso')
+                self.menu()
+            else:
+                print('Saldo insuficiente na carteira, abasteça menos ou aumente seu saldo')
+                self.menu()
+        else:
+            print('Seu tanque está quase cheio, abasteça menos')
+            self.menu()
+
+    def financas(self):
+        opcoes = [
+        inquirer.List(
+            'escolha',
+            message = 'SUA CARTEIRA',
+            choices = ('Saldo', 'Adicionar dinheiro', 'Sacar', 'Voltar')
+            )
+        ]
+
+        escolha = inquirer.prompt(opcoes)['escolha']
+
+        if escolha == 'Saldo':
+            print(f'Valor na carteira: R$ {self.carteira},00')
+
+        elif escolha == 'Adicionar dinheiro':
+            add = int(input('Digite o valor que quer adicionar: R$'))
+            self.carteira += add
+            print('Valor adicionado com sucesso!')
+
+        elif escolha == 'Sacar':
+            saque = int(input('Digite o valor que quer sacar: R$'))
+            self.carteira -= saque
+            print('Valor sacado com sucesso!')
+
+        self.menu()
+
+    def sair(self):
+        sys.exit()
+        
